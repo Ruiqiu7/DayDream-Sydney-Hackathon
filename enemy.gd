90f831ca7174f4f1abe0f9ca4526d1316962fd57
Rachel <153251868+Ruiqiu7@users.noreply.github.com>
2025-09-28 07:08:41 +1000

    Organising

--- enemy.gd
+++ enemy.gd
@@ -1,49 +1,19 @@
 extends CharacterBody2D
 
-var speed = 50
-var velocity = Vector2()
-export var direction = -1
-export var detects_cliffs = true
-signal lost_life
+var direction: int = -1
 
-#Directing the enemy to flip when its direction of movement is changed. 
 func _ready():
 	if direction == 1:
-		$jellyfish.flip_h = true
-	$floorChecker.position.x = $CollisionShape2D.shape.get_extents().x * direction
-	$floorChecker.enabled = detects_cliffs
-	if not detects_cliffs:
-		set_modulate(Color(1.2,0.5,1))
-
-#Directing the enemy to flip when it hits a wall. 
-func _physics_process(delta):
-	if is_on_wall() or not $floorChecker.is_colliding() and detects_cliffs and is_on_floor():
+		$AnimatedSprite2D.flip_h = true
+	
+	
+func _physics_process(_delta: float) -> void:
+	if is_on_wall():
 		direction = direction * -1
-		$jellyfish.flip_h = not $jellyfish.flip_h
-		$floorChecker.position.x = $CollisionShape2D.shape.get_extents().x * direction
-
-#The enemy's gravity and speed.
-	velocity.y = 20
-	velocity.x = speed * direction
+		$AnimatedSprite2D.flip_h = not $AnimatedSprite2D.flip_h
 	
-	velocity = move_and_slide(velocity,Vector2.UP)
-
-func _on_topChecker_body_entered(body):
-	$jellyfish.play("dead")
-	speed = 0 
-	set_collision_layer_bit(4, false)
-	set_collision_mask_bit(0,false)
-	$topChecker.set_collision_layer_bit(4,false)
-	$topChecker.set_collision_mask_bit(0,false)
-	$sideChecker.set_collision_layer_bit(4,false)
-	$sideChecker.set_collision_mask_bit(0,false)
-	$Timer.start()
-	$SoundEnemyOuch.play()
+	velocity.y += 20
 	
-func _on_sideChecker_body_entered(body):
-	body.ouch(position.x)
-	emit_signal("lost_life")
-	$SoundPlayerOuch.play()
-
-func _on_Timer_timeout():
-	queue_free()
+	velocity.x = 100 * direction
+	
+	move_and_slide()
--- player.gd
+++ player.gd
@@ -1,25 +1,32 @@
 extends CharacterBody2D
 
 var coins = 0
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+const SPEED = 500.0
+const JUMP_VELOCITY = -600.0
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	elif Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
+		$AnimatedSprite2D.play("jump")
+			
+	elif Input.is_action_pressed("left"):
+		velocity.x = -SPEED
+		$AnimatedSprite2D.flip_h = true
+		$AnimatedSprite2D.play("run")
+	elif Input.is_action_pressed("right"):
+		velocity.x = SPEED
+		$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.play("run")
+		
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, 50)
+		$AnimatedSprite2D.play("idle")
 
 	move_and_slide()
 	
 func add_coin() :
 	coins = coins +1
-	print(coins)
+	print(coins) 
