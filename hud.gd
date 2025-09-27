4e52e8cf927ce7600c689d243f677207a99808f6
alexander <alex@alexer.au>
2025-09-27 16:56:45 +1000

    Converted coins to global variable

--- hud.gd
+++ hud.gd
@@ -1,27 +1,20 @@
 extends CanvasLayer
 
-var coins = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Coins.text = str(coins) 
+	# Display the initial coin count from the global script
+	update_coin_label()
 
- 
+# A function to update the UI. This is better than calling _ready() again.
+func update_coin_label() -> void:
+	$Coins.text = str(Global.coins)
 
 func _on_coin_collected() -> void:
-	coins = coins + 1
-	_ready()
-
-
-
-
-
-		
-
+	Global.coins += 1 # A shorter way to write Global.coins = Global.coins + 1
+	update_coin_label()
 
 func _on_portal_body_entered(body: Node2D) -> void:
-	if coins >= 3:
-	
-		coins = coins - 3
-		_ready()
-
+	if Global.coins >= 3:
+		Global.coins -= 3 # A shorter way to write Global.coins = Global.coins - 3
+		update_coin_label()
 		get_tree().change_scene_to_file("res://test.tscn")
