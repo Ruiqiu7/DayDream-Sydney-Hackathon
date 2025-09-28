427b980df7050678080f47f81035031bfddc4f49
c8stella <153251802+c8stella@users.noreply.github.com>
2025-09-28 14:26:20 +1000

    Reapply "Merge branch 'main' of https://github.com/Ruiqiu7/DayDream-Sydney-Hackathon"
    This reverts commit befdf108e62621764b9ceee0749fba4ded48200f.

--- sacrifice.gd
+++ sacrifice.gd
@@ -1,11 +1,21 @@
 extends Control
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	# This node doesn't need to do anything when it first loads.
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	# Check if the 'g' key was just pressed this frame.
+	if Input.is_action_pressed("ghost"):
+		# Only subtract a ghost if there are ghosts to remove.
+		if Global.ghost > 0:
+			Global.ghost -= 1
+
+			get_tree().change_scene_to_file(Global.new_level)
+			
+	# Check if the 'h' key was just pressed this frame.
+	if Input.is_action_pressed("life"):
+		Global.take_damage(1)		
+		get_tree().change_scene_to_file(Global.new_level)
