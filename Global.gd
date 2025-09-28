446a00f51fb432a98f1ab23e53025464d0bcf7f1
alexander <alex@alexer.au>
2025-09-28 14:24:56 +1000

    Fixed sacrifice

--- Global.gd
+++ Global.gd
@@ -3,7 +3,7 @@ var coins: int = 0
 var new_level = "res://level_1.tscn"
 var coins_required=3
 var current_level = "res://level_1.tscn"
-var allocated_time = 5
+var allocated_time = 40
 var max_health = 4
 var health = 4
 var ghost = 3
@@ -16,11 +16,13 @@ func _ready() -> void:
 
 func take_damage(amount):
 	health -= amount
-	health = clamp(health, 0, max_health)
+	health = clamp(health, -1, max_health)
 	print(health)
 
 	health_changed.emit()
-	if health = 0:
+	if health == -1:
+		get_tree().change_scene_to_file("gameover.tscn")
+		
 		
 	
 func reset_health():
