d1f4118275f6b75dfbf38fbc0a6fb296a7a133c9
alexander <alex@alexer.au>
2025-09-28 11:54:49 +1000

    Added healthbar

--- hud.gd
+++ hud.gd
@@ -1,14 +1,24 @@
 extends CanvasLayer
 
 @export var time_left = Global.allocated_time # Set the starting time in seconds
+@export var health_textures: Array[Texture2D]
+
 var new_coins =0
 func _ready() -> void:
 	update_coin_label()
+	update_health_bar()
 	update_time_label()
 
 func update_coin_label() -> void:
 	$Coins.text = str(Global.coins+new_coins )
 
+func update_health_bar() -> void:
+	var texture_index = Global.max_health - Global.health
+	
+	if texture_index >= 0 and texture_index < health_textures.size():
+		$HealthBar.texture = health_textures[texture_index]
+	if Global.health <= 0:
+		pass## Game over scene to be implemented
 func update_time_label() -> void:
 	$TimeLabel.text = "Time: %d:%02d" % [time_left / 60, time_left % 60]
 
@@ -33,5 +43,8 @@ func _on_portal_new_level() -> void:
 		get_tree().change_scene_to_file(Global.new_level)
 
 func _on_fall_zone_body_entered(body: Node2D) -> void:
+	Global.take_damage(1)
 	new_coins =0
 	get_tree().change_scene_to_file(Global.current_level)
+	
+	
